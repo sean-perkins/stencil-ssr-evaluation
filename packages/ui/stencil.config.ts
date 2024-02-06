e6d0ed8d4c486d0c2f098f009ea0fcfb9692b509
Sean Perkins <sean@ionic.io>
2024-02-05 22:10:25 -0500

    chore: nextjs simple example client rendering stencil

--- packages/ui/stencil.config.ts
+++ packages/ui/stencil.config.ts
@@ -1,7 +1,8 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
-  namespace: 'ui',
+  namespace: 'components',
   outputTargets: [
     {
       type: 'dist',
@@ -17,6 +18,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    reactOutputTarget({
+      componentCorePackage: 'ui',
+      proxiesFile: '../../apps/nextjs/src/app/proxies.ts',
+      includeImportCustomElements: true,
+      includeDefineCustomElements: false
+    })
   ],
   testing: {
     browserHeadless: "new",
