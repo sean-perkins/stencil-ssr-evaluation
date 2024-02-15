bcaa52b4d389b776b19573722e62b6987e763a1b
Sean Perkins <sean@ionic.io>
2024-02-15 18:50:01 -0500

    chore: render web component in remix

--- apps/remix/app/routes/_index.tsx
+++ apps/remix/app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { MyComponent } from "../proxies";
+import { useEffect } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,11 +10,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  useEffect(() => {
+    import("ui/components/my-component.js").then((x) => {
+      x.defineCustomElement();
+    });
+  });
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
       {/* Uncomment the following line to reproduce the CJS exception */}
       {/* <MyComponent first="John" last="Doe" /> */}
+      <my-component first="John" last="Doe" />
     </div>
   );
 }
